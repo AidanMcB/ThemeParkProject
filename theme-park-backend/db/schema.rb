f2a80249d22cabc64bf6551318149813eddd45cc
AidanMcB <aidankmcbride@gmail.com>
2020-05-04 15:32:36 -0500

    some backend and css

--- theme-park-backend/db/schema.rb
+++ theme-park-backend/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_04_192121) do
+ActiveRecord::Schema.define(version: 2020_05_04_195514) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+  end
 
   create_table "wallets", force: :cascade do |t|
     t.float "money"
--- theme-park-frontend/wallet.js
+++ theme-park-frontend/wallet.js
@@ -1,5 +1,5 @@
 let createWallet = function(){
-    fetch("https:localhost:3000/wallet")
+    fetch("https:localhost:3000/wallets/1")
         .then(function(response){
             return response.json()
         })
@@ -9,12 +9,12 @@ let createWallet = function(){
     //temporary local variable for money
     let money = 0
     //create wallet object and append it to the page
-    let walletContainer = document.createElement("div")
+    // let walletContainer = document.createElement("div")
+    let navbar = document.querySelector(".nav-bar")
     let moneyDisplay = document.createElement("p")
+    moneyDisplay.setAttribute('class', 'money-display')
     moneyDisplay.innerText = `${money} dollars`
 
-
-    walletContainer.append(moneyDisplay)
-    document.body.append(walletContainer)
+    navbar.append(moneyDisplay)
 
 }
