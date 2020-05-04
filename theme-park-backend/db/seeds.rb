2b8e128e96de63d5a1729ca55899ecdf943797b2
AidanMcB <aidankmcbride@gmail.com>
2020-05-04 15:48:09 -0500

    working on fetch request for wallet data

--- theme-park-backend/db/seeds.rb
+++ theme-park-backend/db/seeds.rb
@@ -4,18 +4,9 @@ Button.destroy_all
 ButtonUpgrade.destroy_all
 
 
-<<<<<<< HEAD
-testUser = User.create({user_name: "John Doe"})
-testWallet = Wallet.create({money: 20.00, img_src: "none", user_id: 1})
-
-Button.create({value: 1})
-
-ButtonUpgrade.create({button_id: 1, name: "Your First Upgrade", flavor_text: "Congrats... You're now an idle pro", click_multiplier: 2})
-=======
 ButtonUpgrade.create({button_id: 1, name: "Your First Upgrade", flavor_text: "Congrats... You're now an idle pro", click_multiplier: 2})
 
 
 testUser = User.create({user_name: "John Doe"})
-testWallet = Wallet.create({money: 20.00, img_src: "none", user_id: 1})
+testWallet = Wallet.create({money: 20.00, img_src: "none", user_id: testUser.id})
 
->>>>>>> 97b8cd18db40119c9eb6c03161fcf50fa0577b7c
